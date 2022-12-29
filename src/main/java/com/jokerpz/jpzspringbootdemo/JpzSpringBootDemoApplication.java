f1da1f134f9dfa97aa3002688d67f235cbace3c4
joker-mac <lpinzhou@163.com>
2022-12-30 00:50:05 +0800

    Initial commit

---
+++ src/main/java/com/jokerpz/jpzspringbootdemo/JpzSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jokerpz.jpzspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpzSpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpzSpringBootDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jokerpz/jpzspringbootdemo/JpzSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jokerpz.jpzspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpzSpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
